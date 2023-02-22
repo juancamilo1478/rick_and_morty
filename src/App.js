829024123b510c9bf01d039e773715051485ad7a
juancamilo1478 <juancamilo1478@gmail.com>
2023-02-21 22:47:05 -0500

    borrador

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import  estilo  from'./App.module.css'
 import Cards from './components/card/Cards'
 import SearchBar from './components/searchBar/SearchBar'
 import Portada from './components/Portada'
-import { useState,useEffect } from 'react'
-import { Routes,Route ,useLocation,useNavigate} from 'react-router-dom'
+// import { useState } from 'react'
+import { Routes,Route ,useLocation  } from 'react-router-dom'
 import Detail from './components/card/Detail'
 import Form from './components/Form'
 import Favorites from './components/favorites/favorites'
 
 function App () {
 const lugar=useLocation()
-const navigate = useNavigate();
-const [access, setAccess] = useState(false);
-const username = 'ejemplo@gmail.com';
-const password = '1password';
+// const navigate = useNavigate();
+// const [access, setAccess] = useState(false);
+// const username = 'ejemplo@gmail.com';
+// const password = '1password';
 
 
-function login(userData) {
-   if (userData.password === password && userData.username === username) {
-      setAccess(true);
-      navigate('/home');
-   }
-}
+// function login(userData) {
+//    if (userData.password === password && userData.username === username) {
+//       setAccess(true);
+//       navigate('/home');
+//    }
+// }
 
 // useEffect(() => {
 //    !access && navigate('/');
@@ -34,7 +34,7 @@ if(lugar.pathname==='/' || lugar.pathname==='/home')
     <Route path='/home' element={<Portada/>}/>
     <Route path='/cartas' element={<Cards />} />  
     <Route path='/detail/:detailId'  element={<Detail/>}/>
-    <Route path='/' element={<Form   login={login }/>} />
+    <Route path='/' element={<Form   />} />
     </Routes> </ div >)
 }
   else {return (<div className={estilo.contenido}>
